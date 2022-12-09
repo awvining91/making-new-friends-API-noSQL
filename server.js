9a47b46ece39e0758228c2a3c2c47c46689b16cd
Will Vining <awvining91@gmail.com>
2022-12-08 20:14:38 -0700

    finished server.js

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3007;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/making-new-friends-API-noSQL', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+});
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`You are now connected at localhost:${PORT}`));
+
